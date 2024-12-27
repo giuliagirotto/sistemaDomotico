5288568d6fb7797b36f1382621575f6164f87f29
mankai0 <87978582+mankai0@users.noreply.github.com>
2024-12-27 19:44:42 +0000

    Update DeviceCP.cpp

---
+++ src/DeviceCP.cpp
@@ -0,0 +1,33 @@
+#include "DeviceCP.h"
+#include <iostream>
+#include "Logger.h"
+
+extern Logger logger; // Logger globally available
+
+DeviceCP::DeviceCP(const std::string& name, int id, double power, int duration)
+    : Device(name, id, power), duration(duration), startTime(0) {}
+
+void DeviceCP::turnOn() {
+    if (!isOn) {
+        isOn = true;
+        logger.logEvent("Il dispositivo '" + name + "' si è acceso.");
+    }
+}
+
+void DeviceCP::turnOff() {
+    if (isOn) {
+        isOn = false;
+        logger.logEvent("Il dispositivo '" + name + "' si è spento.");
+    }
+}
+
+void DeviceCP::updateTimer(int currentTime) {
+    // Only start the device when the current time matches the start time
+    if (startTime == currentTime) {
+        turnOn();
+    }
+    // If the current time reaches the end of the duration, turn off the device
+    if (isOn && currentTime >= startTime + duration) {
+        turnOff();
+    }
+}
