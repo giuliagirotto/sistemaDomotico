5fadba70005f99019a0b7bdd74e8df950f4b04b9
silviamarcato <silviamarcatoo@gmail.com>
2025-01-09 23:18:06 +0100

    Update Controller.h

--- include/Controller.h
+++ include/Controller.h
@@ -3,21 +3,45 @@
 
 #include <vector>
 #include <memory>
+#include <string>
 #include "Device.h"
+
+class Device;
+class Logger;
 
 class Controller {
 private:
     std::vector<std::unique_ptr<Device>> devices;
-    double maxPower;
-    double solarProduction;
-    int currentTime;
+    stdm:shared_ptr<Logger> logger;
+    double maxPower; //potenza massima 
+    double solarProduction; //produzione solare
+    double currentPower; //potenza attuale
+    std::string currentTime; //orario attuale
+
+    void enforcePowerLimit(); //applica il limite della potenza
 
 public:
-    Controller(double maxPower, double solarProduction);
-    void addDevice(std::unique_ptr<Device> device);
-    void setTime(int newTime);
-    void handleOverload();
-    void showStatus();
+    Controller(double maxPower, double solarProduction); //costruttore
+    ~Controller (); //distruttore
+    
+    //gestione dispositivi
+    void addDevice(const std::shared_ptr<Device> device);
+    void turnOn(const std::string& deviceName);
+    void turnOff(const std::string& deviceName);
+
+    //gestione timer
+    void setDeviceTimer(const std::string& deviceName, const std::string& startTime, const std::string& stopTime);
+    void removeDeviceTimer(const std::string& deviceName);
+
+    //monitoraggio devices
+    void showDevices() const;
+    void showDevice(const std::string& deviceName) const;
+
+    //gestione del tempo
+    void setTime(const std::string& time);
+    void resetTime();
+    void resetTimers();
+    void resetAll();
 };
 
-#endif // CONTROLLER_H
+#endif //CONTROLLER_H
