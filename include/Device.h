9be5dff2b19bf7174961596c5bc0dd77560f8bb9
mankai0 <87978582+mankai0@users.noreply.github.com>
2025-01-14 14:57:46 +0000

    Update Device.h

--- include/Device.h
+++ include/Device.h
@@ -10,6 +10,8 @@ protected:
     int id;
     double power;
     double totalEnergy;
+    std::string tempStartTime;
+    std::string tempEndTime;
     bool isOn;
     Controller& controller;
 
@@ -19,8 +21,8 @@ public:
 
     virtual void turnOn() = 0;
     virtual void turnOff() = 0;
-    virtual void checkTimer(int currentTime) = 0;
-    virtual void setTimer(startTime, endTime) = 0;
+    virtual void checkTimer(std::string currentTime) = 0;
+    virtual void setTimer(std::string startTime,std::string endTime) = 0;
     virtual void removeTimer() = 0;
 
     const std::string& getName() const;
@@ -28,6 +30,10 @@ public:
 
     double getInstantPower() const;
     double getTotalEnergyConsumed() const;
+    std::string getTempStartTime() const;
+    std::string getTempEndTime() const;
+    void setTempStartTime(const std::string& startingTime);
+    void setTempEndTime(const std::string& endingTime);
     
 };
 
