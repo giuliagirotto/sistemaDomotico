39c33f1c0a4e7fb3635a006c635d097534d4dfde
mankai0 <87978582+mankai0@users.noreply.github.com>
2025-01-14 13:15:34 +0000

    Update Device.cpp

--- src/Device.cpp
+++ src/Device.cpp
@@ -1,8 +1,8 @@
 #include "Device.h"
 #include <iostream>
 
-Device::Device(const std::string& name, int id, double power)
-    : name(name), id(id), power(power), isOn(false) {}
+Device::Device(const std::string& name, int id, double power, Controller& controller)
+    : name(name), id(id), power(power), Controller(controller) ,isOn(false) {}
 
 const std::string& Device::getName() const {
     return name;
