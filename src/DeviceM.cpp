9a2c18bd33cd297f74214b80cc8636babbaa75e5
mankai0 <87978582+mankai0@users.noreply.github.com>
2025-01-14 13:20:57 +0000

    Update DeviceM.cpp

--- src/DeviceM.cpp
+++ src/DeviceM.cpp
@@ -1,8 +1,6 @@
 #include "DeviceM.h"
 #include <iostream>
-#include "Logger.h"
 
-extern Logger log; // Logger globally available
 
 DeviceM::DeviceM(const std::string& name, int id, double power)
     : Device(name, id, power, controller), stopTime(0) {}
@@ -28,9 +26,9 @@ void DeviceM::checkTimer(std::string currentTime) {
     // Manual devices do not have automatic timers, so no action needed here
 
     if (startTime == currentTime) {
-        turnOn();
+        controller.turnOn(this->getName());
     }
     if (stopTime == currentTime) {
-        turnOff();
+        controller.turnOff(this->getName());
     }
 }
