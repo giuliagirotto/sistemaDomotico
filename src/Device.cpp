1e94382941450816326a6b9593ecb37134433fee
mankai0 <87978582+mankai0@users.noreply.github.com>
2025-01-14 21:51:37 +0000

    Update Device.cpp

--- src/Device.cpp
+++ src/Device.cpp
@@ -1,3 +1,4 @@
+// Samuele Caushaj 2123698
 #include "Device.h"
 #include "Controller.h"
 #include <iostream>
@@ -5,21 +6,27 @@
 Device::Device(const std::string& name, int id, double power, Controller& controller)
     : name(name), id(id), power(power), controller(controller) , totalEnergy(0), timeOn(0), isOn(false) {}
 
+//recupera nome dispositivo
 const std::string& Device::getName() const {
     return name;
 }
 
+//accende dispositivo
 bool Device::isDeviceOn() const {
     return isOn;
 }
 
+//recupera la potenza in KWH
 double Device::getInstantPower() {
     return power;
 }
 
+// Calcola l'energia totale
 void Device::totalEnergyConsumed(){
-    totalEnergy = power * timeOn / 60;
+    totalEnergy = power * timeOn / 60; // potenza in KWH * tempo in minuti che il dispositivo e stato acceso / 60 minuti.
 }
+
+// recupera l'energia
 double Device::getTotalEnergyConsumed() {
     totalEnergyConsumed();
     return totalEnergy;
